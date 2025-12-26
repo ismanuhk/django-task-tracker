bcd795952843397713374a6d3fde41b10f9df47b
study <ismanuhk@gmail.com>
2025-12-26 13:11:19 +0530

    Fixed logout 405 by using POST form

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('students.urls')),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
     path('', include('django.contrib.auth.urls')),
 ]
